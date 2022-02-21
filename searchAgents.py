0ef0e27c43032118f8acbbf547c7f63ed0a06d2e
manexsora <98772019+manexsora@users.noreply.github.com>
2022-02-21 09:59:34 +0100

    ej 5

--- searchAgents.py
+++ searchAgents.py
@@ -277,21 +277,26 @@ class CornersProblem(search.SearchProblem):
         # Please add any code here which you would like to use
         # in initializing the problem
         "*** YOUR CODE HERE ***"
-
     def getStartState(self):
         """
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        esquinas = {}
+        for c in self.corners:
+            esquinas[c] = False
+        return (self.startingPosition, esquinas)
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        for c in self.corners:
+            if not state[1][c]:
+                return False
+        return True
 
     def getSuccessors(self, state):
         """
@@ -308,15 +313,28 @@ class CornersProblem(search.SearchProblem):
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
             # Add a successor state to the successor list if the action is legal
             # Here's a code snippet for figuring out whether a new position hits a wall:
+            
             #   x,y = currentPosition
             #   dx, dy = Actions.directionToVector(action)
             #   nextx, nexty = int(x + dx), int(y + dy)
             #   hitsWall = self.walls[nextx][nexty]
-
+        
             "*** YOUR CODE HERE ***"
-
-        self._expanded += 1 # DO NOT CHANGE
+            x,y = state[0]
+            esquinas = dict(state[1])
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
+            if not hitsWall:
+                
+                if (nextx,nexty) in self.corners:
+                    esquinas[(nextx,nexty)] = True
+                    
+                successors.append((((nextx,nexty),esquinas), action, 1))
+                
         return successors
+                    
+            
 
     def getCostOfActions(self, actions):
         """
