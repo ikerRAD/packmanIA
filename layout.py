c6aa723f1b6d257a2ae79498efb73c9dd35adac5
manexsora <98772019+manexsora@users.noreply.github.com>
2022-02-23 10:52:24 +0100

    Update layout.py

--- layout.py
+++ layout.py
@@ -7,7 +7,8 @@ import random
 from functools import reduce
 
 VISIBILITY_MATRIX_CACHE = {}
-
+#for i in o
+#a
 class Layout:
     """
     A Layout manages the static information about the game board.
